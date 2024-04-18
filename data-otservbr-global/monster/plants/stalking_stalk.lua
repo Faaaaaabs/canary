2f15d5db913c30ea489dc5450a136cd84c748324
Faaaaaabs <servidorflc@gmail.com>
2024-04-18 20:04:19 -0300

    Update stalking_stalk.lua

--- data-otservbr-global/monster/plants/stalking_stalk.lua
+++ data-otservbr-global/monster/plants/stalking_stalk.lua
@@ -100,8 +100,8 @@ monster.defenses = {
 }
 
 monster.elements = {
-	{ type = COMBAT_PHYSICALDAMAGE, percent = -25 },
-	{ type = COMBAT_ENERGYDAMAGE, percent = 0 },
+	{ type = COMBAT_PHYSICALDAMAGE, percent = -10 },
+	{ type = COMBAT_ENERGYDAMAGE, percent = -15 },
 	{ type = COMBAT_EARTHDAMAGE, percent = 25 },
 	{ type = COMBAT_FIREDAMAGE, percent = 25 },
 	{ type = COMBAT_LIFEDRAIN, percent = 0 },
